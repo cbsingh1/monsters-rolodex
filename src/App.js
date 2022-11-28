eda984b7f2e04c1efed758dbd11d7f016247a305
Chandra Bhan Singh <cbhansingh1@gmail.com>
2022-11-28 14:36:08 +0800

    Basic changes, as per new format

--- src/App.js
+++ src/App.js
@@ -6,40 +6,36 @@ import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
-
   constructor() {
     super();
 
     this.state = {
-      monsters : [],
-      searchField: ''
+      monsters: [],
+      searchField: '',
     };
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState( { monsters : users} ));
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
-
     const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => 
-        monster.name.toLowerCase().includes(searchField.toLowerCase())
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
 
-    return(
-      <div className="App">
+    return (
+      <div className='App'>
         <h1> Monster Rolodex </h1>
-        <SearchBox 
-          placeholder='search monsters' 
-          handleChange={e => this.setState({ searchField: e.target.value })}
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
         />
-        
         <CardList monsters={filteredMonsters} />
-        
-    </div>
+      </div>
     );
   }
 }
